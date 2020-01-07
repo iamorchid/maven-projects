96faa2ce8b71db018a1d5528fc818a9271c70015
Will Zhang <jian.zhang4@envision-digital.com>
2020-01-07 18:07:49 +0800

    add http examples

---
+++ http/src/main/java/HttpAsyncClientTest.java
@@ -0,0 +1,97 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.http.Consts;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
+import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
+import org.apache.http.impl.nio.reactor.IOReactorConfig;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.nio.reactor.ConnectingIOReactor;
+import org.apache.http.util.EntityUtils;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class HttpAsyncClientTest {
+
+    public static void main(String[] args) throws Exception {
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(50000)
+                .setSocketTimeout(50000)
+                .setConnectionRequestTimeout(1000)
+                .build();
+
+        IOReactorConfig ioReactorConfig = IOReactorConfig.custom().
+                setIoThreadCount(Runtime.getRuntime().availableProcessors())
+                .setSoKeepAlive(true)
+                .build();
+        ConnectingIOReactor ioReactor = new DefaultConnectingIOReactor(ioReactorConfig);
+        PoolingNHttpClientConnectionManager connManager = new PoolingNHttpClientConnectionManager(ioReactor);
+        connManager.setMaxTotal(100);
+        connManager.setDefaultMaxPerRoute(100);
+
+        final CloseableHttpAsyncClient client = HttpAsyncClients.custom().
+                setConnectionManager(connManager)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        //start
+        client.start();
+
+        List<NameValuePair> tokenParams = new ArrayList<>(5);
+        tokenParams.add(new BasicNameValuePair("env", "beta"));
+        tokenParams.add(new BasicNameValuePair("keys",
+                "iot-mqtt-broker.measurepoint.topic,iot-mqtt-broker.bull.name"));
+        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(tokenParams, Consts.UTF_8);
+        HttpGet get = new HttpGet("http://10.27.20.20:8090/config2/get?" + EntityUtils.toString(entity));
+
+        client.execute(get, new CallBack());
+
+        System.out.println("print any key to exit ...");
+        System.in.read();
+
+        client.close();
+    }
+
+    static class CallBack implements FutureCallback<HttpResponse> {
+        @Override
+        public void completed(HttpResponse httpResponse) {
+            try {
+                HttpEntity rspEntity = httpResponse.getEntity();
+                Gson gson = new GsonBuilder().create();
+                ContentType contentType = ContentType.getOrDefault(rspEntity);
+                Charset charset = contentType.getCharset();
+                try (Reader reader = new InputStreamReader(rspEntity.getContent(), charset)) {
+                    System.out.println("received: \n" + gson.fromJson(reader, Map.class));
+                }
+            } catch (Exception e) {
+                System.err.println("captured error: " + e.getMessage());
+            }
+        }
+
+        @Override
+        public void failed(Exception e) {
+            System.err.println("request failed with error: " + e.getMessage());
+        }
+
+        @Override
+        public void cancelled() {
+            System.err.println("request cancelled");
+        }
+    }
+
+}
