1943b2b38c2882e7440057cd6c92236421c4222a
Will Zhang <jian.zhang4@envision-digital.com>
2020-02-14 14:16:09 +0800

    update micrometer module

--- micrometer/src/main/java/will/test/mircometer/Application.java
+++ micrometer/src/main/java/will/test/mircometer/Application.java
@@ -1,5 +1,9 @@
 package will.test.mircometer;
 
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.config.MeterFilterReply;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +11,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        Metrics.globalRegistry.config().meterFilter(new MeterFilter() {
+            @Override
+            public MeterFilterReply accept(Meter.Id id) {
+                System.out.println("id: " + id);
+                return MeterFilterReply.ACCEPT;
+            }
+        });
+
         SpringApplication springApplication = new SpringApplication(Application.class);
         springApplication.run(args);
         System.out.println("application started");
--- micrometer/src/main/java/will/test/mircometer/SimpleController.java
+++ micrometer/src/main/java/will/test/mircometer/SimpleController.java
@@ -1,24 +1,63 @@
 package will.test.mircometer;
 
+import io.micrometer.core.instrument.*;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.config.MeterFilterReply;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class SimpleController {
 
+    static final AtomicInteger GUAGE = Metrics.gauge("gauge number", new AtomicInteger(0));
+
+    static final Counter GetNameRequestCounter = Metrics.counter("request_counter", "path", "getName");
+    static final Timer GetNameTimer = Timer
+            .builder("duration timer")
+            .tag("path", "getName")
+            .publishPercentileHistogram()
+            .minimumExpectedValue(Duration.ofMillis(10))
+            .maximumExpectedValue(Duration.ofMillis(3000))
+            .register(Metrics.globalRegistry);
+
+    static final Counter GetNameWatingRequestCounter = Metrics.counter("request_counter", "path", "getNameWaiting");
+    static final Timer GetNameWatingTimer = Timer
+            .builder("duration timer")
+            .tag("path", "getNameWaiting")
+            .publishPercentileHistogram()
+            .minimumExpectedValue(Duration.ofMillis(10))
+            .maximumExpectedValue(Duration.ofMillis(3000))
+            .register(Metrics.globalRegistry);
+
+    static final DistributionSummary summary = DistributionSummary
+            .builder("distribution")
+            .description("simple distribution summary")
+            .minimumExpectedValue(10L)
+            .maximumExpectedValue(3000L)
+            .publishPercentileHistogram()
+            .register(Metrics.globalRegistry);
+
     @RequestMapping("/getName")
-    public String getName(){
+    public String getName() {
+        GetNameRequestCounter.increment();
+        int duration = new Random().nextInt(1000);
+        GetNameTimer.record(Duration.ofMillis(duration));
+        summary.record(duration);
+        GUAGE.set(new Random().nextInt(5));
         return "prometheus-demo";
     }
 
     @RequestMapping("/getNameWaiting")
-    public String getNameWaiting(){
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return "prometheus-demo-waiting";
+    public String getNameWaiting() {
+        GetNameWatingRequestCounter.increment();
+        int duration = new Random().nextInt(5000);
+        GetNameWatingTimer.record(Duration.ofMillis(duration));
+        summary.record(duration);
+        GUAGE.set(new Random().nextInt(5));
+        return "prometheus-demo";
     }
-
 }
